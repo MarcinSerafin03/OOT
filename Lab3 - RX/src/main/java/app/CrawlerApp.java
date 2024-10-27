e673783609ac43017f734f81d1cba66eb346219e
Marcin Serafin <Marcin.serafin0325@gmail.com>
2024-10-27 18:39:38 +0100

    Lab3 + two files Lab2

---
+++ Lab3 - RX/src/main/java/app/CrawlerApp.java
@@ -0,0 +1,21 @@
+package app;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class CrawlerApp {
+
+    public static final String SCRAPER_API_KEY = "fef0f2cdd0a7445156cc60f5e45ee5bf";
+
+    private static final List<String> TOPICS = List.of("Doctor who", "Agent Cooper", "Sherlock", "Poirot", "Detective Monk");
+
+
+    public static void main(String[] args) throws IOException {
+        PhotoCrawler photoCrawler = new PhotoCrawler();
+        photoCrawler.resetLibrary();
+//        photoCrawler.downloadPhotoExamples();
+        photoCrawler.downloadPhotosForQuery(TOPICS.get(0));
+//        photoCrawler.downloadPhotosForMultipleQueries(TOPICS);
+    }
+}
---
+++ Lab3 - RX/src/main/java/app/PhotoCrawler.java
@@ -0,0 +1,57 @@
+package app;
+
+import model.Photo;
+import util.PhotoDownloader;
+import util.PhotoProcessor;
+import util.PhotoSerializer;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class PhotoCrawler {
+
+    private static final Logger log = Logger.getLogger(PhotoCrawler.class.getName());
+
+    private final PhotoDownloader photoDownloader;
+
+    private final PhotoSerializer photoSerializer;
+
+    private final PhotoProcessor photoProcessor;
+
+    public PhotoCrawler() throws IOException {
+        this.photoDownloader = new PhotoDownloader();
+        this.photoSerializer = new PhotoSerializer("./photos");
+        this.photoProcessor = new PhotoProcessor();
+    }
+
+    public void resetLibrary() throws IOException {
+        photoSerializer.deleteLibraryContents();
+    }
+
+    public void downloadPhotoExamples() {
+        try {
+            photoDownloader.getPhotoExamples()
+                   .subscribe(photoSerializer::savePhoto,
+                            throwable -> log.log(Level.WARNING, "Could not download a photo", throwable));
+        } catch (IOException e) {
+            log.log(Level.WARNING, "Could not download a photo", e);
+        }
+
+    }
+
+    public void downloadPhotosForQuery(String query) throws IOException {
+        try {
+            photoDownloader.searchForPhotos(query)
+                    .take(10)
+                    .subscribe(photoSerializer::savePhoto, throwable -> log.log(Level.WARNING, "Could not download a photo 3", throwable));
+        } catch (InterruptedException e){
+            log.log(Level.WARNING, "Could not download a photo 4", e);
+        }
+    }
+
+    public void downloadPhotosForMultipleQueries(List<String> queries) {
+        // TODO Implement me :(
+    }
+}
